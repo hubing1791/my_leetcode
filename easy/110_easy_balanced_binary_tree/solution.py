555074a6a82d13526cfd1a45a15a4e123607202b
hubing1791 <benrenbucun@qq.com>
2021-01-03 00:24:14 +0800

    s

---
+++ easy/110_easy_balanced_binary_tree/solution.py
@@ -0,0 +1,20 @@
+# https://leetcode-cn.com/problems/balanced-binary-tree/
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def tree_height(node: TreeNode):
+            if not node:
+                return 0
+            left_height = tree_height(node.left)
+            right_height = tree_height(node.right)
+            if left_height == -1 or right_height == -1 or abs(left_height - right_height) > 1:
+                return -1
+            return max(left_height, right_height) + 1
+
+        return tree_height(root) >= 0
