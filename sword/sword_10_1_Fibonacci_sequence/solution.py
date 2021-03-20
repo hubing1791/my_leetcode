b67819bc7ad5066df685a688b838217687ab8b43
hubing1791 <benrenbucun@qq.com>
2021-03-20 16:34:21 +0800

    s

--- sword/sword_10_1_Fibonacci_sequence/solution.py
+++ sword/sword_10_1_Fibonacci_sequence/solution.py
@@ -2,3 +2,20 @@
 
 class Solution:
     def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        f0 = 0
+        f1 = 1
+        for i in range(1, n):
+            print(i)
+            f_temp = f1
+            f1 += f0
+            f0 = f_temp
+            if i % 10 == 0:
+                f0, f1 = f0 % 1000000007, f1 % 1000000007
+        return f1 % 1000000007
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.fib(2))
