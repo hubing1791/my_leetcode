16715bad59df33e04eb1c8fcfae824ff55d50f11
hubing1791 <benrenbucun@qq.com>
2021-03-24 16:04:06 +0800

    solution

---
+++ sword/sword_16_integer_power_of_number/solution.py
@@ -0,0 +1,19 @@
+# https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/
+
+class Solution:
+    # 幂的快速算法，看的解答
+    def myPow(self, x: float, n: int) -> float:
+        if x == 0:
+            return 0
+        if n < 0:
+            flag = 0
+            n = -n
+        else:
+            flag = 1
+        result = 1
+        while n:
+            if n & 1 == 1:
+                result *= x
+            n >>= 1
+            x *= x
+        return result if flag else 1/result
