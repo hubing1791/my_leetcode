917e27e4f0f034aa5092346c0941c078ca46d9dd
peng <benrenbucun@qq.com>
2021-04-29 16:25:24 +0800

    s

--- medium/143_medium_reorder_list/solution.py
+++ medium/143_medium_reorder_list/solution.py
@@ -19,7 +19,7 @@ class Solution:
                     fast = fast.next
             else:
                 break
-        pre,cur = fast.next,slow
+        pre, cur = fast.next, slow
         while cur:
             if not cur.next:
                 rev_half = cur
@@ -27,17 +27,53 @@ class Solution:
             cur.next = pre
             pre = cur
             cur = temp
-        cur_pre,cur_back = head,rev_half
+        cur_pre, cur_back = head, rev_half
         while cur_back:
             cur_pre_temp = cur_pre.next
             cur_back_temp = cur_back.next
             cur_pre.next = cur_back
             cur_back.next = cur_pre_temp
-            cur_pre, cur_back = cur_pre_temp,cur_back_temp
+            cur_pre, cur_back = cur_pre_temp, cur_back_temp
 
-    # 参考答案的第一班
+    # 参考答案的第一版本
     def reorderList_1(self, head: ListNode) -> None:
         new_nodehead = head
         node_list = []
         while new_nodehead:
-             
+            node_list.append(new_nodehead)
+            new_nodehead = new_nodehead.next
+        leng_list = len(node_list)
+        for i in range(leng_list // 2 - 1):
+            node_list[i].next = node_list[-i - 1]
+            node_list[-i - 1].next = node_list[i + 1]
+        midd = leng_list // 2
+        # 把最后几个
+        if leng_list % 2 == 0:
+            node_list[midd - 1].next = node_list[-midd]
+            node_list[-midd].next = None
+        else:
+            node_list[midd - 1].next = node_list[-midd]
+            node_list[-midd].next = node_list[midd]
+            node_list[midd].next = None
+
+    # 参考答案的第二版本
+    def reorderList_2(self, head: ListNode) -> None:
+        slow, fast = head, head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        cur, pre = slow.next, None
+        # 把前后链断开
+        slow.next = None
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        # 这样结束后后半段的头就是此时的pre
+        cur_left, cur_right = head, pre
+        while cur_right:
+            temp_left, temp_right = cur_left.next, cur_right.next
+            cur_left.next, cur_right.next = cur_right, temp_left
+            cur_left, cur_right = temp_left, temp_right
---
+++ medium/470_medium_implement_rand10_using_rand7/solution.py
@@ -0,0 +1,14 @@
+import random
+
+
+def rand7():
+    return random.randint(1, 7)
+
+
+class Solution:
+    def rand10(self):
+        randx = 7 * (rand7() - 1) + rand7()
+        if randx <= 40:
+            return randx % 10 + 1
+        else:
+            return self.rand10()
