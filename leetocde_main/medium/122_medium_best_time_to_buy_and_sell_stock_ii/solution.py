55717b069dbcaac4333aee7a09a14f53ad28167c
hubing1791 <benrenbucun@qq.com>
2021-10-29 10:41:58 +0800

    s

--- leetocde_main/medium/122_medium_best_time_to_buy_and_sell_stock_ii/solution.py
+++ leetocde_main/medium/122_medium_best_time_to_buy_and_sell_stock_ii/solution.py
@@ -6,3 +6,10 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        price_prev = prices[0]
+        for price in prices[1:]:
+            if price > price_prev:
+                result += price - price_prev
+            price_prev = price
+        return result
