5bc4503946883eed3a16f04bd539789ae2aecef8
hubing1791 <benrenbucun@qq.com>
2021-11-18 18:40:54 +0800

    2021-11-18

---
+++ leetocde_main/medium/131_medium_palindrome_partitioning/solution.py
@@ -0,0 +1,8 @@
+# https://leetcode-cn.com/problems/palindrome-partitioning/
+# 2021-11-08
+
+from typing import List
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = 
